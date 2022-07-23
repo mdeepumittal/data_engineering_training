bb47e6716e4fdd84262e03054d7a7d0d8de078ec
Vishesh Garg <vishesh.garg@napster.com>
2022-07-23 14:03:47 +0530

    new file added

---
+++ car game.py
@@ -0,0 +1,34 @@
+command=""
+started=False
+while True:
+    command=input('>').lower()
+    if command=="start":
+        if started:
+            print('car is already started!')
+        else:
+            started=True
+            print('car started')
+    elif command =="stop":
+        if not started:
+            print('car is already stopped')
+        else:
+            started=False
+            print('car stopped')
+    elif command =="help":
+       print( """
+start-  to start a car
+stop- to stop a car
+quit - end a game
+    """)
+    elif command== 'quit':
+         break
+    else:
+        print('i dont understand')
+
+
+
+
+
+
+
+
